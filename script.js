12f5593ee0a201df28849560440b67579cdcd13e
Danish624-bash <danishmurtaza624@gmail.com>
2025-02-23 23:17:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("onclick").addEventListener("click", function() {
+    alert("Introduction: Hi! my Name is Danish I am from Kashmir I am desgine the website from introduction and my dream is a big company fronted develpor job thinks for wotching my website" );
+});
+
+document.getElementById("logo").addEventListener("click", function(){
+    alert("google.come");
+});
